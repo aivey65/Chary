5afd029b9bab83ec8430b22fe2d1c94ab01771bb
Asha Ivey <ashavivey@gmail.com>
2022-08-24 18:23:56 -0500

    Working on navigation bar.

--- static/javascript/chary-nav.js
+++
@@ -1,8 +0,0 @@
-// <chary-nav> navbar component
-class CharyNav extends HTMLElement {
-    connectedCallback() {
-        this.textContent = 'Hello, World!';
-    }
-}
-
-customElements.define('chary-nav', CharyNav);
