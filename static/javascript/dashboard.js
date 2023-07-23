e6a3732ee956e53aff90bc915f9591525b415126
Asha <39443120+aivey65@users.noreply.github.com>
2023-07-23 14:25:45 -0500

    Starting charts stuff

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -548,3 +548,49 @@ function generateOverviewEarnings() {
     overviewEarningContainer.append(overviewHeader, addButton, earningTable);
     return overviewEarningContainer;
 }
+
+///////////////////////////////////
+// Chart Configurating Functions //
+///////////////////////////////////
+function totalBudgetsAndAmounts() {
+    const data = [
+        [
+            { year: 2010, count: 10 },
+            { year: 2011, count: 20 },
+            { year: 2012, count: 15 },
+            { year: 2013, count: 25 },
+            { year: 2014, count: 22 },
+            { year: 2015, count: 30 },
+            { year: 2016, count: 28 },
+        ],
+    ]
+}
+
+function expensesPerMonth() {
+    const data = [
+        [
+            { year: 2010, count: 10 },
+            { year: 2011, count: 20 },
+            { year: 2012, count: 15 },
+            { year: 2013, count: 25 },
+            { year: 2014, count: 22 },
+            { year: 2015, count: 30 },
+            { year: 2016, count: 28 },
+        ],
+    ]
+}
+
+function totalBudgetsAndAmounts() {
+    
+    const data = [
+        [
+            { year: 2010, count: 10 },
+            { year: 2011, count: 20 },
+            { year: 2012, count: 15 },
+            { year: 2013, count: 25 },
+            { year: 2014, count: 22 },
+            { year: 2015, count: 30 },
+            { year: 2016, count: 28 },
+        ],
+    ]
+}
