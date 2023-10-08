e02f34ebd8acdf20943d8c6a7b3f36d0ff630a0b
Asha <39443120+aivey65@users.noreply.github.com>
2023-10-08 18:08:42 -0500

    Charts on budget page switch back and forth properly, added a plugin for doughnut text

--- static/javascript/budget.js
+++ static/javascript/budget.js
@@ -14,7 +14,7 @@ function loadBudget(id) {
         const currentChart = generateVariousCharts(chartData, 0, 1);
         const limitedChartsContainer = document.createElement("div");
         limitedChartsContainer.append(currentChart);
-        limitedChartsContainer.classList.add("limited-charts-container");
+        limitedChartsContainer.id = "limited-charts-container";
 
         // Create a container for limited charts to switch around
         const chartContainer = document.getElementById('details-chart-container');
@@ -23,10 +23,6 @@ function loadBudget(id) {
         
         const dotCarousel = carouselButtons(chartData, "details-chart-dots", "limited-charts-container", 1, generateVariousCharts);
         chartContainer.append(dotCarousel);
-        
-        document.getElementById('details-name').textContent = budget.name;
-        document.getElementById('fraction-top').textContent = responseData.currency + formatNumber(budget.usedAmount);
-        document.getElementById('fraction-bottom').textContent = responseData.currency + formatNumber(budget.amount);
         
         if (budget.description == "") {
             document.getElementById('details-description').textContent = "None";
@@ -93,7 +89,18 @@ function generateVariousCharts(items, slideNum, maxShow) {
                 }],
                 labels: data.labels,
             },
+            plugins: [
+                doughnutText
+            ],
             options: {
+                elements: {
+                    center: {
+                        text: data.data[0] + "\n／\n" + data.data[1],
+                        color: COLORS_LIGHT,
+                        minFontSize: 25, // Default is 20 (in px), set to false and text will not wrap.
+                        lineHeight: 25 // Default is 25 (in px), used for when text wraps
+                    }
+                },
                 maintainAspectRatio: false,
                 responsive: true,
                 plugins: {
@@ -101,7 +108,7 @@ function generateVariousCharts(items, slideNum, maxShow) {
                         callbacks: {
                             label: function(context) {
                                 let dataObject = context.dataset;
-                                return dataObject.label + ": " + user_currency + dataObject.data[context.dataIndex];
+                                return user_currency + dataObject.data[context.dataIndex];
                             }
                         }
                     }
@@ -230,5 +237,90 @@ function budgetUsePerPeriod(period, expenses) {
         { year: 2014, count: 22 },
         { year: 2015, count: 30 },
         { year: 2016, count: 28 },
-      ];
+    ];
 }
+// Plugin credited to stackoverflow users: https://stackoverflow.com/questions/20966817/how-to-add-text-inside-the-doughnut-chart-using-chart-js
+const doughnutText = {
+    id: 'doughnut-centertext',
+    beforeDraw: function(chart) {
+        if (chart.config.options.elements.hasOwnProperty("center")) {
+            // Get ctx from string
+            var ctx = chart.ctx;
+
+            // Get options from the center object in options
+            var centerConfig = chart.config.options.elements.center;
+            var fontStyle = 'Kanit';
+            var txt = centerConfig.text;
+            var color = centerConfig.color || '#000';
+            var maxFontSize = centerConfig.maxFontSize || 30;
+            var sidePadding = centerConfig.sidePadding || 20;
+            var sidePaddingCalculated = (sidePadding / 100) * (chart._metasets[chart._metasets.length-1].data[0].innerRadius * 2)
+            // Start with a base font of 30px
+            ctx.font = "30px " + fontStyle;
+
+            // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+            var stringWidth = ctx.measureText(txt).width;
+            var elementWidth = (chart._metasets[chart._metasets.length-1].data[0].innerRadius * 2) - sidePaddingCalculated;            
+
+            // Find out how much the font can grow in width.
+            var widthRatio = elementWidth / stringWidth;
+            var newFontSize = Math.floor(30 * widthRatio);
+            var elementHeight = (chart._metasets[chart._metasets.length-1].data[0].innerRadius * 2);
+
+            // Pick a new font size so it will not be larger than the height of label.
+            var fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
+            var minFontSize = centerConfig.minFontSize;
+            var lineHeight = centerConfig.lineHeight || 25;
+            var wrapText = false;
+
+            if (minFontSize === undefined) {
+                minFontSize = 20;
+            }
+
+            if (minFontSize && fontSizeToUse < minFontSize) {
+                fontSizeToUse = minFontSize;
+                wrapText = true;
+            }
+
+            // Set font settings to draw it correctly.
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+            var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+            ctx.font = fontSizeToUse + "px " + fontStyle;
+            ctx.fillStyle = color;
+
+            if (!wrapText) {
+                ctx.fillText(txt, centerX, centerY);
+                return;
+            }
+
+            var words = txt.split(' ');
+            var line = '';
+            var lines = [];
+
+            // Break words up into multiple lines if necessary
+            for (var n = 0; n < words.length; n++) {
+                var testLine = line + words[n] + ' ';
+                var metrics = ctx.measureText(testLine);
+                var testWidth = metrics.width;
+                if (testWidth > elementWidth && n > 0) {
+                    lines.push(line);
+                    line = words[n] + ' ';
+                } else {
+                    line = testLine;
+                }
+            }
+
+            // Move the center up depending on line height and number of lines
+            centerY -= (lines.length / 2) * lineHeight;
+
+            for (var n = 0; n < lines.length; n++) {
+                ctx.fillText(lines[n], centerX, centerY);
+                centerY += lineHeight;
+            }
+            //Draw text in center
+            ctx.fillText(line, centerX, centerY);
+        }
+    }
+};
